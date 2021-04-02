ff1cdc87c4c1c93b8b18954d9b88b2cb99a56594
Ekaterina Pashkina <ufo9108@gmail.com>
2021-04-02 18:15:38 +0300

    +progress-dialog

--- ActivityDialog.java
+++ ActivityDialog.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class ActivityDialog extends AppCompatActivity implements View.OnClickListener{
 
     TextView time, date;
+    Button btn_round;
+    Button btn_line;
     MyTimeAndDateDialogFragment mtdf;
+    MyProgressDialogFragment mpdf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +24,32 @@ public class ActivityDialog extends AppCompatActivity implements View.OnClickLis
 
         time = (TextView) findViewById(R.id.textView);
         date = (TextView) findViewById(R.id.textView2);
+        btn_round = findViewById(R.id.button2);
+        btn_line = findViewById(R.id.button3);
 
         time.setOnClickListener(this);
         date.setOnClickListener(this);
+        btn_round.setOnClickListener(this);
+        btn_line.setOnClickListener(this);
 
         mtdf = new MyTimeAndDateDialogFragment(time, date, this);
+        mpdf = new MyProgressDialogFragment(this);
     }
 
     @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
-        mtdf.onClick(v);
+        int id = v.getId();
+        switch (id){
+            case R.id.textView:
+            case R.id.textView2:
+                mtdf.onClick(v);
+                break;
+            case R.id.button2:
+            case R.id.button3:
+                mpdf.onClick(v);
+                break;
+        }
     }
 
     public void onClickShowDialog(View view) {
@@ -49,7 +68,5 @@ public class ActivityDialog extends AppCompatActivity implements View.OnClickLis
         Toast.makeText(getApplicationContext(), "You choose the button" +
                 "---On pause---", Toast.LENGTH_LONG).show();
     }
-
-
 
 }
